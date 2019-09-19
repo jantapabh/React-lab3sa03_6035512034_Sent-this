f53ad470dec0e671dc1b8ce09c7e2d38ce92f47c
Jantapa Janjao <49370136+jantapabh@users.noreply.github.com>
2019-09-20 00:06:50 +0700

    Add files via upload

---
+++ src/Start.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom'
+import './Start.css';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+import App from './App'
+import './App.css';
+
+
+function Start() {
+  return (
+    <div className="one">
+      <div className="row">
+        <h1 className="joa">Let's play Now !</h1>
+      </div>
+    </div>
+  );
+}
+export default Start;
